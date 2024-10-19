a54008e529c973cced11829dfb94c828a4097b94
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2024-10-19 16:58:35 +0300

    daisyui

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,48 +1,69 @@
+import { Field, Form, Formik } from "formik";
+
 export default function Registration() {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
+          <h1 className="text-5xl font-bold">Sign up now!</h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus,
+            recusandae ratione? Obcaecati praesentium repudiandae itaque
+            repellat quia eveniet ipsam, inventore repellendus velit veritatis,
+            quo dicta consequuntur blanditiis dolorem optio magni! Sapiente
+            deleniti distinctio, dolor labore quia odio itaque. Minima delectus
+            obcaecati pariatur fugiat doloribus quidem iure expedita provident,
+            corrupti iusto temporibus minus? Quod deleniti porro asperiores,
+            quidem reiciendis ex esse!
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </form>
+          <Formik>
+            <Form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <Field
+                  name="name"
+                  type="text"
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <Field
+                  name="email"
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label"></label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary" type="submit">
+                  Register
+                </button>
+              </div>
+            </Form>
+          </Formik>
         </div>
       </div>
     </div>
